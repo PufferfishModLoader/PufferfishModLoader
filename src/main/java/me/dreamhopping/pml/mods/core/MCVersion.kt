4f45c67bfb24b37a8b9708caf98d56cbf91182a2
Conor Byrne <71222289+dreamhopping@users.noreply.github.com>
2021-04-22 21:45:06 +0100

    [Mods] Add MCVersion enum

---
+++ src/main/java/me/dreamhopping/pml/mods/core/MCVersion.kt
@@ -0,0 +1,14 @@
+package me.dreamhopping.pml.mods.core
+
+enum class MCVersion(public val version: String) {
+    V1_8_9("1.8.9"),
+    V1_9_4("1.9.4"),
+    V1_10_2("1.10.2"),
+    V1_11("1.11"),
+    V1_12("1.12"),
+    V1_13_2("1.13.2"),
+    V1_14_4("1.14.4"),
+    V1_15_2("1.15.2"),
+    V1_16_4("1.16.4"),
+    ALL_VERSIONS("ALL");
+}
